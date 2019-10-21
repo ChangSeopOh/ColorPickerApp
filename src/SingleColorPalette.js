44444e5313e462fc8cf750de1daa6ed467a0b8c4
Changseop OH <jaden.oh@yahoo.com>
2019-10-21 09:58:27 -0700

    Move out Styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,49 +4,7 @@ import Navbar from './Navbar';
 import {Link} from 'react-router-dom';
 import PaletteFooter from './PaletteFooter';
 import {withStyles} from '@material-ui/styles';
-
-const styles={
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors:{
-        height: "90%"
-    },
-    goBack:{
-        width:"20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px", 
-        opacity: 1  ,
-        backgroundColor: "black",
-        "& a" : {
-            width: "100px",
-            height:"30px",
-            position: "absolute",
-            display:"inline-block",
-            top:"50%",
-            left:"50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(255,255,255, 0.3)",
-            fontSize:"1rem",
-            lineHeight: "30px",
-            color:"white",
-            textTransform: "uppercase",
-            border: "none",
-            cursor: "pointer"
-        }
-        
-    } 
-};
-
+import styles from './styles/PaletteStyles';
 
 class SingleColorPalette extends Component {
     constructor(props){
@@ -92,9 +50,11 @@ class SingleColorPalette extends Component {
                 <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
                 <div className={classes.colors}>
                     {colorBoxes}
-                    <div className={classes.goBack}>
-                        <Link to={`/palette/${id}`}>Go Back</Link>
-                    </div>
+                        <div className={classes.goBack}>
+                            <Link to={`/palette/${id}`}>
+                                Go Back
+                            </Link>
+                        </div>
                 </div> 
                 <PaletteFooter paletteName={paletteName} emoji={emoji}/>
             </div>
